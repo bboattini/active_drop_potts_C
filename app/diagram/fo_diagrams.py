c7da5098d42dc06a98692ecba7598fbec7ecbf92
bboattini <b.boattini@gmail.com>
2025-02-15 13:01:05 -0300

    fo_diagrams addapted to dissertation images

--- app/diagram/fo_diagrams.py
+++ app/diagram/fo_diagrams.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import app.aux_func as af
+#from app.MSD.MSD_plot import G_DICT
 
 PATH = str(os.path.abspath(__file__))
 PARAM = {'vert': 'a', 
          'hori': 'h',
          'multi': 'CI',
          'var': 'theta'}
+IC_DICT = {'CB': 'CB', 'WE': 'WE1'}
 
 def Diagram_Generator(var = PARAM['var'], config=None):
     print("-----------------------------------Start----------------------------------\n")
@@ -26,13 +28,18 @@ def Diagram_Generator(var = PARAM['var'], config=None):
     if var == 'theta':
         var_dir = "peak_detect"
         y_limits = (90, 160)
-        labeling_y = r'$\theta_c$'
+        labeling_y = r'$\theta_c$(°)'
         leg_loc = "lower right"
     elif var == 'Vp':
         var_dir = "histograms"
         y_limits = (0, 0.2)
-        labeling_y = r"$V_f$"
+        labeling_y = r"$f$"
         leg_loc = "upper right"
+    elif var == 'e':
+        var_dir = "peak_detect"
+        y_limits = (0, 1)
+        labeling_y = r"$e$"
+        leg_loc = "lower right"
     else:
         print("Invalid 'var' parameter")
         return
@@ -44,6 +51,11 @@ def Diagram_Generator(var = PARAM['var'], config=None):
     # Find the files inside path that ends in .txt
     files = os.listdir(path)
     files_to_read = [f for f in files if f.endswith('.txt') and f.startswith(var)]
+    # remove the value 10 and 6 from a_values
+    files_to_read = [f for f in files_to_read if 'a_6' not in f and 'a_10' not in f]
+    '''files_to_read = [f for f in files_to_read if 'a_7' not in f and 'a_8' not in f]
+    files_to_read = [f for f in files_to_read if 'a_9' not in f]'''
+
     if len(files_to_read) > 0:
         print(f"Found {len(files_to_read)} files")
     else:
@@ -51,8 +63,15 @@ def Diagram_Generator(var = PARAM['var'], config=None):
         return()
 
     # Sort the parameters list from lower to greater
-    h_values = np.sort(np.unique(np.array([h_from_file(f) for f in files_to_read])))
-    a_values = np.sort(np.unique(np.array([a_from_file(f) for f in files_to_read])))
+    h_values = np.sort(np.unique(np.array([float(h_from_file(f)) for f in files_to_read])))
+    a_values = np.sort(np.unique(np.array([float(a_from_file(f)) for f in files_to_read])))
+    
+    # remove de value 8 from a_values
+    a_values = np.delete(a_values, np.where(a_values == 6))
+    #a_values = np.delete(a_values, np.where(a_values == 7))
+    #a_values = np.delete(a_values, np.where(a_values == 9))
+    a_values = np.delete(a_values, np.where(a_values == 10))
+    print(a_values)
     fo_values = np.sort(np.unique(np.array([0 for f in files_to_read])))
 
     config_dict = {'a': a_values, 'h': h_values, 'fo': fo_values, 'CI': np.array(["CB", "WE"])}
@@ -62,23 +81,27 @@ def Diagram_Generator(var = PARAM['var'], config=None):
 
     #--------------------------------------------------------------------------------------
     # Create a subplot
-    plt.rcParams.update({'font.size': 15})
+    plt.rcParams.update({'font.size': 20})
     fig, axs = plt.subplots(len(vertical), len(horizontal), figsize=(int(len(horizontal)*5), int(len(vertical)*4)))
     legend_added = {}
     #--------------------------------------------------------------------------------------
     # File loop
     files = os.listdir(PATH.replace("fo_diagrams.py", ""))
     files = [f for f in files if f.endswith('.txt')]
+    files  = [f for f in files if f.endswith('.txt') and not f.startswith('a6') and not f.startswith('a10')]
+    #files  = [f for f in files if f.endswith('.txt') and not f.startswith('a7') and not f.startswith('a9')]
+    #files  = [f for f in files if f.endswith('.txt') and not f.startswith('a8')]
+    print(files)
     flag = 0
     for f in files:
-        if var == 'theta' and 'a' in [PARAM['vert'], PARAM['hori']]:
+        if 'a' in [PARAM['vert'], PARAM['hori']]:
             if 'a' == PARAM['vert']:
                 vert_index = float(f.split("_")[-2].replace("a", ""))
                 hori_index = 10
             else:
                 hori_index = float(f.split("_")[-2].replace("a", ""))
                 vert_index = 10
-        elif var == 'theta' and 'CI' in [PARAM['vert'], PARAM['hori']]:
+        elif 'CI' in [PARAM['vert'], PARAM['hori']]:
             if 'CI' == PARAM['vert']:
                 vert_index = vertical[flag]
                 hori_index = 10
@@ -105,12 +128,12 @@ def Diagram_Generator(var = PARAM['var'], config=None):
         #legend_added_for_this_ax = legend_added.setdefault(ax, set())
         legend_added[ax] = []
 
-        if flag == 0:
+        if flag == 0 and var == 'theta':
             # Load the minima file
             minima = np.loadtxt(PATH.replace("fo_diagrams.py", "")+f, unpack=True)
             if minima.ndim == 0:
                 minima = [minima.item()]  # Convert to a list with a single element
-            x = np.arange(0, 10.5)
+            x = np.arange(0, 12)
             for m in minima:
                 ax.plot(x, m*np.ones(len(x)), color='black', alpha=0.5, linewidth=1)
         if flag == 2:
@@ -120,9 +143,10 @@ def Diagram_Generator(var = PARAM['var'], config=None):
         if 'CI' not in [PARAM['vert'], PARAM['hori']]:
             vert_index = float(index_finder(PARAM['vert'], f))
             hori_index = float(index_finder(PARAM['hori'], f))
-
-            ax = find_ax(axs, vert_index, hori_index, vertical, horizontal)
-
+            '''if hori_index != 8:
+                ax = find_ax(axs, vert_index, hori_index, vertical, horizontal)'''
+        if hori_index == 6 or hori_index == 10:
+            continue 
         print(f"for {PARAM['hori']} = {hori_index} and {PARAM['vert']} = {vert_index}")
 
         for line in open(path+"/"+f):
@@ -135,9 +159,11 @@ def Diagram_Generator(var = PARAM['var'], config=None):
                     vert_index = float(index_finder(PARAM['vert'], f))
                     hori_index = state
                 multi_index = float(index_finder(PARAM['multi'], f))
-                ax = find_ax(axs, vert_index, hori_index, vertical, horizontal)
             else:
                 multi_index = state
+                vert_index = float(index_finder(PARAM['vert'], f))
+                hori_index = float(index_finder(PARAM['hori'], f))
+            ax = find_ax(axs, vert_index, hori_index, vertical, horizontal)
             means = np.fromstring(means_str.replace("[","").replace("]",""), sep=' ')
             stds = np.fromstring(stds_str.replace("[","").replace("]",""), sep=' ')
             fo = float(fo)
@@ -148,15 +174,21 @@ def Diagram_Generator(var = PARAM['var'], config=None):
                     means = means[np.argsort(means)[::-1]]
                     # Restrict the number of means to 1
                     means = [means[0]]
+                    # if len(means) > 2:
+                    #    means = [means[0], (means[1]+means[2])/2]
                     for idx, m in enumerate(means):                    
                         if f"{af.COLORS[color_idx]}{idx}" not in legend_added[ax]:
                             #ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=af.MARKERS[color_idx], color=af.COLORS[color_idx], ecolor=af.COLORS[color_idx], elinewidth=1, capsize=0, label=f'{c} '+r'$\theta$'+f'{idx+1}', alpha=1/(idx+1))
+                            ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=af.MARKERS[color_idx], color=af.COLORS[color_idx], ecolor=af.COLORS[color_idx], elinewidth=1, capsize=0, label='IC = '+IC_DICT[c], alpha=1/(idx+1))
+                            # markerfacecolor='none' to make the marker transparent
                             #legend_added_for_this_ax.add(f'{af.COLORS[color_idx]}{idx}')
                             legend_added[ax].append(f'{af.COLORS[color_idx]}{idx}')
                             selected = np.where(np.array(list(legend_added[ax])) == f'{af.COLORS[color_idx]}{idx}')[0][0]
-                            ax.plot(fo, m, color=af.COLORS[selected], linewidth=1)
-                            ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=".", color=af.COLORS[selected], ecolor=af.COLORS[selected], elinewidth=1, capsize=0, label=f"{c} "+r'$\theta$'+f'{idx+1}')
-                            ax.set_title(f"{PARAM['hori']} = {hori_index} e {PARAM['vert']} = {vert_index}")
+                            ax.plot(fo, m, color=af.COLORS[selected], linewidth=5)
+                            #ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=".", color=af.COLORS[selected], ecolor=af.COLORS[selected], elinewidth=1, capsize=0, label=f"{c} "+r'$\theta_c$')
+                            #ax.set_title(f"{PARAM['hori']} = {hori_index} and {PARAM['vert']} = {vert_index}")
+                            ax.set_title(f"{PARAM['hori']} = {hori_index}")
+                            #ax.set_title(f"{G_DICT[str(int(hori_index))]}")
                             ax.set_ylabel(labeling_y)
                             ax.set_xlabel(r'$\mu$')
                             ax.set_xlim(0, 10.5)
@@ -164,10 +196,11 @@ def Diagram_Generator(var = PARAM['var'], config=None):
                             # location down right
                             ax.legend(loc=leg_loc, fontsize='small')
                         else:
-                            #ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=af.MARKERS[color_idx], color=af.COLORS[color_idx], ecolor=af.COLORS[color_idx], elinewidth=1, capsize=0, alpha=1/(idx+1))
+                            ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=af.MARKERS[color_idx], color=af.COLORS[color_idx], ecolor=af.COLORS[color_idx], elinewidth=1, capsize=0, alpha=1/(idx+1))
+                            # markerfacecolor='none' to make the marker transparent
                             selected = np.where(np.array(list(legend_added[ax])) == f'{af.COLORS[color_idx]}{idx}')[0][0]
-                            ax.plot(fo, m, color=af.COLORS[selected], linewidth=1)
-                            ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=".", color=af.COLORS[selected], ecolor=af.COLORS[selected], elinewidth=1, capsize=0)
+                            ax.plot(fo, m, color=af.COLORS[selected], linewidth=5)
+                            #ax.errorbar(fo, m, yerr=np.abs(stds[idx]), fmt=".", color=af.COLORS[selected], ecolor=af.COLORS[selected], elinewidth=1, capsize=0)
         
             
         # add space between suplots
@@ -178,7 +211,7 @@ def Diagram_Generator(var = PARAM['var'], config=None):
     save_dir = str(os.path.abspath(__file__)) 
     save_dir = save_dir.replace('/'+save_dir.split('/')[-1], "")
     fig.tight_layout()
-    fig.savefig(f"{save_dir}/{var}_mu_diagram_{PARAM['vert']}-vs-{PARAM['hori']}_RESTRICTED.jpg", format='jpeg')
+    fig.savefig(f"{save_dir}/{var}_mu_diagram_{PARAM['vert']}-vs-{PARAM['hori']}.jpg", format='jpeg')
     print("-----------------------------------End------------------------------------\n")
     return()
 
@@ -234,6 +267,9 @@ def find_ax(axs, vert_index, hori_index, vertical, horizontal):
     return ax
 
 if __name__ == '__main__':
-    #Diagram_Generator(var='Vp', config = ('h', 'a'))
+    Diagram_Generator(var='Vp', config = ('h', 'a'))
+    #Diagram_Generator(var='Vp', config = ('h', 'CI'))
     Diagram_Generator(var='theta', config = ('h', 'a'))
-    Diagram_Generator(var='theta', config = ('h', 'CI'))
+    #Diagram_Generator(var='theta', config = ('h', 'CI'))
+    #Diagram_Generator(var='e', config = ('h', 'a'))
+    #Diagram_Generator(var='e', config = ('h', 'CI'))
